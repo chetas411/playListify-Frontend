5115d46a6854cdba2249cb3c7c9be24bc0e88332
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-20 22:01:44 +0530

    set up for tailwind css done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-        theSpotifyProject begins....
-    </div>
+    <h1 className="text-3xl font-bold">
+      Tailwind CSS
+    </h1>
   )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/main.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,document.getElementById('root')
 );
 
