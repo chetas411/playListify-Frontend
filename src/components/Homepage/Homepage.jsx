121cbb34934520da3c4b6ea184e28a530c92d5f1
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-21 20:29:34 +0530

    feat: box shadow and hover class added on button

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
         <div className="w-full pt-36 h-screen flex flex-wrap flex-col space-y-8 justify-start items-center">
             <Header />
             <Showcase />
-            <button className="w-48 py-2 px-7 text-2xl rounded bg-green-500">Log In</button>
+            <button className="w-48 py-2 px-7 text-2xl rounded bg-green-400 hover:bg-green-500 shadow-2xl">Log In</button>
         </div>
     )
 }
