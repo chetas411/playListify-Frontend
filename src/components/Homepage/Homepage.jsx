f29fb4924af6c484284508b73e97a8ad73e365f4
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-22 14:24:18 +0530

    func: login auth url linked

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header/Header';
 import Showcase from './Showcase/Showcase';
 
+// spotify authorisation URL
+const spotify_auth_url = process.env.REACT_APP_SPOTIFY_AUTH_URL;
+
 const Homepage = () => {
     return (
         <div className="w-full pt-36 h-screen flex flex-wrap flex-col space-y-8 justify-start items-center">
             <Header />
             <Showcase />
-            <button className="w-48 py-2 px-7 text-2xl rounded bg-green-400 hover:bg-green-500 shadow-2xl">Log In</button>
+            <a href={spotify_auth_url}><button  type="button" className="w-48 py-2 px-7 text-2xl rounded bg-green-400 hover:bg-green-500 focus:outline-none shadow-2xl">Log In</button></a>
         </div>
     )
 }
