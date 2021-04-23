6c71214f7561e8fb65ab5e0a6965cf5c0af73922
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-23 22:22:50 +0530

    topshowcase in section created

--- src/components/Mainpage/Section/Section.jsx
+++ src/components/Mainpage/Section/Section.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import TopShowcase from './TopShowcase/TopShowcase';
 
 const Section = () => {
     return (
         <div className="h-screen col-span-4 lg:col-span-6 xl:col-span-8 bg-gray-800">
-            {/* TopShowcase */}
+            <TopShowcase />
             {/* BottomShowcase */}
         </div>
     )
