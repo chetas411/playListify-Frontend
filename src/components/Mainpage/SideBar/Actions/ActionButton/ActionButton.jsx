2fe48fd5250b15e56016666544c00b11a6a87e16
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-23 15:14:09 +0530

    feat: active state added to actions

--- src/components/Mainpage/SideBar/Actions/ActionButton/ActionButton.jsx
+++ src/components/Mainpage/SideBar/Actions/ActionButton/ActionButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const ActionButton = (props) => {
+    let colorclass = props.activeclass[props.ckey];
     return (
-        <div className=" group flex flex-col justify-center items-center w-full h-20 space-y-1 cursor-pointer">
+        <div className=" group flex flex-col justify-center items-center w-full h-20 space-y-1 cursor-pointer focus:bg-red-300"
+            onClick={()=>props.update(props.ckey)}
+        >
             {props.children}
-            <h1 className="text-base text-gray-900 group-hover:text-green-400">{props.action}</h1>
+            <h1 className={`text-base text-gray-900 group-hover:text-green-400 ${colorclass}`}>{props.action}</h1>
         </div>
     )
 }
--- src/components/Mainpage/SideBar/Actions/Actions.jsx
+++ src/components/Mainpage/SideBar/Actions/Actions.jsx
@@ -1,19 +1,39 @@
+import React,{useState} from 'react';
 import ActionButton from './ActionButton/ActionButton';
 import {HeartIcon} from '@heroicons/react/outline';
 import {StarIcon} from '@heroicons/react/outline';
 import {ClockIcon} from '@heroicons/react/outline';
 
 const Actions = () => {
+    const [activeBtn,setActiveBtn] = useState({
+        tracks: "text-green-400",
+        artists: "",
+        history: ""
+    });
+
+    const toggleActiveBtn = (key)=>{
+        let updateActiveBtn = {...activeBtn};
+        for(const property in updateActiveBtn){
+            if(property === key){
+                updateActiveBtn[key] = "text-green-400";
+            }
+            else{
+                updateActiveBtn[property] = "";
+            }
+        }
+        setActiveBtn(updateActiveBtn);
+    }
+
     return (
         <div className="flex flex-col justify-center items-center w-full space-y-4">
-            <ActionButton action="Top Tracks">
-                <HeartIcon className="h-10 w-12 text-gray-900 group-hover:text-green-400" />
+            <ActionButton ckey="tracks" update={toggleActiveBtn} activeclass={activeBtn} action="Top Tracks">
+                <HeartIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.tracks}`} />
             </ActionButton>
-            <ActionButton action="Top Artists">
-                <StarIcon className="h-10 w-12 text-gray-900 group-hover:text-green-400" />
+            <ActionButton ckey="artists" update={toggleActiveBtn} activeclass={activeBtn} action="Top Artists">
+                <StarIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.artists}`} />
             </ActionButton>
-            <ActionButton action="History">
-                <ClockIcon className="h-10 w-12 text-gray-900 group-hover:text-green-400" />
+            <ActionButton ckey="history" update={toggleActiveBtn} activeclass={activeBtn} action="History">
+                <ClockIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.history}`} />
             </ActionButton>
         </div>
     )
