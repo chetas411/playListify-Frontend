d6e399a78230ad2f2ad5ac42da37f4f91ca056cf
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-24 14:13:37 +0530

    created context for image sharing between sidebar and section

--- src/components/Mainpage/Mainpage.jsx
+++ src/components/Mainpage/Mainpage.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React,{useState,createContext} from 'react';
 import SideBar from './SideBar/SideBar';
 import Section from './Section/Section';
+import cImg from '../../assets/images/pexels-miguel-á-padriñán-3391932.jpg';
+
+const DisplayDataContext = createContext();
 
 const Mainpage = () => {
+    const [sectionData,setSectiondata] = useState({
+        imgUrl: cImg,
+        text: "Top Tracks"
+    });
+
+    const updateSectionData = (data) =>{
+        setSectiondata({
+            imgUrl: data.imgUrl,
+            text: data.text
+        });
+    };
+
+    const displayValue = {
+        sectionData,
+        setSectiondata,
+        updateSectionData
+    }
+
     return (
         <div className="w-full h-screen grid grid-cols-5 lg:grid-cols-7 xl:grid-cols-9">
-            <SideBar />
-            <Section />
+            <DisplayDataContext.Provider value={displayValue}>
+                <SideBar />
+                <Section />
+            </DisplayDataContext.Provider>
         </div>
     )
 }
 
+export {DisplayDataContext};
 export default Mainpage;
