5542246a812a15c9cafd6e44307822c91e9c6019
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-24 14:16:51 +0530

    feat: background changing wih change in active button

--- src/components/Mainpage/SideBar/Actions/ActionButton/ActionButton.jsx
+++ src/components/Mainpage/SideBar/Actions/ActionButton/ActionButton.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {DisplayDataContext} from '../../../Mainpage';
 
 const ActionButton = (props) => {
+    const displaydata = useContext(DisplayDataContext);
+    const handleClick = ()=>{
+        props.update(props.ckey);
+        let newData = {
+            imgUrl: props.bgUrl,
+            text: props.action
+        };
+        displaydata.updateSectionData(newData);
+    }
     let colorclass = props.activeclass[props.ckey];
     return (
         <div className=" group flex flex-col justify-center items-center w-full h-20 space-y-1 cursor-pointer focus:bg-red-300"
-            onClick={()=>props.update(props.ckey)}
+            onClick={handleClick}
         >
             {props.children}
             <h1 className={`text-base text-gray-900 group-hover:text-green-400 ${colorclass}`}>{props.action}</h1>
--- src/components/Mainpage/SideBar/Actions/Actions.jsx
+++ src/components/Mainpage/SideBar/Actions/Actions.jsx
@@ -3,6 +3,9 @@ import ActionButton from './ActionButton/ActionButton';
 import {HeartIcon} from '@heroicons/react/outline';
 import {StarIcon} from '@heroicons/react/outline';
 import {ClockIcon} from '@heroicons/react/outline';
+import bgforTopTracks from '../../../../assets/images/pexels-miguel-á-padriñán-3391932.jpg';
+import bgforTopArtists from '../../../../assets/images/pexels-sebastian-ervi-1763075.jpg';
+import bgforHistory from '../../../../assets/images/pexels-daniel-reche-3721941.jpg';
 
 const Actions = () => {
     const [activeBtn,setActiveBtn] = useState({
@@ -10,7 +13,7 @@ const Actions = () => {
         artists: "",
         history: ""
     });
-
+    
     const toggleActiveBtn = (key)=>{
         let updateActiveBtn = {...activeBtn};
         for(const property in updateActiveBtn){
@@ -22,17 +25,18 @@ const Actions = () => {
             }
         }
         setActiveBtn(updateActiveBtn);
+
     }
 
     return (
         <div className="flex flex-col justify-center items-center w-full space-y-4">
-            <ActionButton ckey="tracks" update={toggleActiveBtn} activeclass={activeBtn} action="Top Tracks">
+            <ActionButton bgUrl={bgforTopTracks} ckey="tracks" update={toggleActiveBtn} activeclass={activeBtn} action="Top Tracks">
                 <HeartIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.tracks}`} />
             </ActionButton>
-            <ActionButton ckey="artists" update={toggleActiveBtn} activeclass={activeBtn} action="Top Artists">
+            <ActionButton bgUrl={bgforTopArtists} ckey="artists" update={toggleActiveBtn} activeclass={activeBtn} action="Top Artists">
                 <StarIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.artists}`} />
             </ActionButton>
-            <ActionButton ckey="history" update={toggleActiveBtn} activeclass={activeBtn} action="History">
+            <ActionButton bgUrl={bgforHistory} ckey="history" update={toggleActiveBtn} activeclass={activeBtn} action="History">
                 <ClockIcon className={`h-10 w-12 text-gray-900 group-hover:text-green-400 ${activeBtn.history}`} />
             </ActionButton>
         </div>
