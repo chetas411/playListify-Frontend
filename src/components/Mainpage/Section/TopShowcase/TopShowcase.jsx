8010f7181127a6ec7523ae0dd0e475d437975053
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-24 14:15:40 +0530

    rendering image through context in section

--- src/components/Mainpage/Section/TopShowcase/TopShowcase.jsx
+++ src/components/Mainpage/Section/TopShowcase/TopShowcase.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import TopBackground from '../../../../assets/images/pexels-miguel-á-padriñán-3391932.jpg'; 
+import React,{useContext} from 'react';
+import {DisplayDataContext} from '../../Mainpage';
 
 const TopShowcase = () => {
+    const {sectionData} = useContext(DisplayDataContext);
     return (
-        <div className=" relative w-full h-3/5 bg-center bg-cover" style={{backgroundImage: `url(${TopBackground})`}}>
-            <h1 className="absolute left-4 bottom-4 text-6xl text-white">Top Artists</h1>
+        <div className=" relative w-full h-3/5 bg-center bg-cover" style={{backgroundImage: `url(${sectionData.imgUrl})`}}>
+            <h1 className="absolute left-4 bottom-4 text-6xl text-white">{sectionData.text}</h1>
             <button className="absolute right-4 top-4 py-2 px-4 text-lg bg-green-400 hover:bg-green-500 text-white rounded focus:outline-none">Create Playlist</button>
         </div>
     )
