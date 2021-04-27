7b3ae98ead347873e09e42bbd3fadfbc227240a8
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-27 15:13:10 +0530

    feat: play track preview on click

--- src/components/Mainpage/Sections/Section/BottomShowcase/BottomShowcase.jsx
+++ src/components/Mainpage/Sections/Section/BottomShowcase/BottomShowcase.jsx
@@ -9,7 +9,7 @@ const BottomShowcase = (props) => {
         <div className="w-full grid grid-cols-5 gap-y-3">
             {
                 info.map((track,index)=>{
-                    return <Card key={index} cover={track.imgUrl.url} name={track.name} artist={track.information.join(", ")} />
+                    return <Card key={index} trackey={`${props.ukey}${index}`} cover={track.imgUrl.url} name={track.name} artist={track.information.join(", ")} preview={track.preview_url} />
                 })
             }
         </div>
--- src/components/Mainpage/Sections/Section/BottomShowcase/Card/Card.jsx
+++ src/components/Mainpage/Sections/Section/BottomShowcase/Card/Card.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
-
 const Card = (props) => {
+    
+    const playTrack = ()=>{
+        let sound = document.getElementById(props.trackey)
+        sound.play();
+    };
+    const pauseTrack = ()=>{
+        let sound = document.getElementById(props.trackey)
+        sound.pause();
+    }
+
     return (
         <div 
             style={{ backgroundImage: `url(${props.cover})`}}
             className="relative w-64 h-64 bg-blend-luminosity bg-cover bg-center cursor-pointer hover:bg-green-400 transition-all transform hover:scale-105 "
+            onClick = {playTrack}
+            onMouseLeave = {pauseTrack}
         >
             <div className="absolute top-0 mt-20 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900"></div>
             <div className="absolute left-1 bottom-1 w-full py-1 px-1 box-border text-white">
                 <h1 className="text-lg">{props.name}</h1>
                 <h1 className="text-sm text-green-400">{props.artist}</h1>
             </div>
+            <audio id={props.trackey}>
+                <source src={props.preview} type="audio/mpeg" />
+            </audio>
         </div>
     )
 }
