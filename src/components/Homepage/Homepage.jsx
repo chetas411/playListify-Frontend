80ef695c7fce9097d1f3b98cec945e120381c40f
Chetas Sharma <sharmachetas.111@gmail.com>
2021-04-29 20:39:57 +0530

    fix: href link

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
             <div className="absolute box-border left-1/2 top-1/4 transform -translate-x-1/2 translate-y-1/2 w-full flex flex-col space-y-4 sm:space-y-8 justify-items-center items-center">
                 <Header />
                 <Showcase />
-                <button type="button" className="w-24 py-1 px-4 text-lg sm:w-48 sm:py-2 sm:px-7 sm:text-2xl rounded bg-green-400 hover:bg-green-500 focus:outline-none shadow-2xl"><a href={spotify_auth_url}>Log In</a></button>
+                <a href={spotify_auth_url}><button type="button" className="w-24 py-1 px-4 text-lg sm:w-48 sm:py-2 sm:px-7 sm:text-2xl rounded bg-green-400 hover:bg-green-500 focus:outline-none shadow-2xl">Log In</button></a>
             </div>
         </div>
     )
